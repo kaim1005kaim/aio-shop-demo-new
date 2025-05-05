da6fce68098d8867f2b9c396223584458677f8db
kaim1005kaim <kai.moriguchi1005@gmail.com>
2025-05-05 21:24:48 +0900

    Fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+// src/App.tsx (修正後・全文)
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Layout from './components/layout/Layout';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage'; // HomePageをインポート
 import CategoryPage from './pages/CategoryPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import TechnicalPage from './pages/TechnicalPage';
 import DiagnosticTool from './pages/DiagnosticTool';
-import AllProductsPage from './pages/AllProductsPage';
+import AllProductsPage from './pages/AllProductsPage'; // AllProductsPageもインポート
 import { AIOptimizationProvider } from './contexts/AIOptimizationContext';
 import { LocalizationProvider } from './contexts/LocalizationContext';
 
@@ -19,8 +21,16 @@ function App() {
           <Router>
             <Layout>
               <Routes>
-                <Route path="/" element={<AllProductsPage />} />
-                <Route path="/home" element={<HomePage />} />
+                {/* ↓↓↓ ルートパスの修正 ↓↓↓ */}
+                <Route path="/" element={<HomePage />} />
+                {/* ↑↑↑ ルートパス ("/") に HomePage を表示 */}
+
+                {/* 必要であれば、AllProductsPage に新しいパスを割り当て */}
+                <Route path="/products" element={<AllProductsPage />} />
+
+                {/* /home パスは不要になる可能性が高いのでコメントアウトまたは削除 */}
+                {/* <Route path="/home" element={<HomePage />} /> */}
+
                 <Route path="/category/:categoryId" element={<CategoryPage />} />
                 <Route path="/product/:productId" element={<ProductDetailPage />} />
                 <Route path="/tech-explanation" element={<TechnicalPage />} />
